4d95e3fa9ba5fd8cf319d54b885c842fa4912246
Oleg <boxmilo@gmail.com>
2019-08-07 08:52:07 +0600

    asset

---
+++ src/Assetic/Factory/Worker/MergeWorker.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * This file is part of the Assetic package, an OpenSky project.
+ *
+ * (c) 2010-2014 OpenSky Project Inc
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Assetic\Factory\Worker;
+
+use Assetic\Asset\AssetCollectionInterface;
+use Assetic\Asset\AssetInterface;
+use Assetic\Factory\AssetFactory;
+
+/**
+ * Adds cache busting code
+ *
+ * @author Kris Wallsmith <kris.wallsmith@gmail.com>
+ */
+class MergeWorker implements WorkerInterface
+{
+    private $separator;
+
+    public function __construct($separator = '-')
+    {
+        $this->separator = $separator;
+    }
+
+    public function process(AssetInterface $asset, AssetFactory $factory)
+    {
+        if($asset instanceof \Assetic\Asset\AssetReference){
+            $asset = clone $asset;
+        }
+        echo  $asset->getTargetPath(). get_class($asset).PHP_EOL;
+        if($asset instanceof \Assetic\Asset\AssetCollectionInterface){
+            foreach ($asset as $leaf) {
+                echo  $leaf->getTargetPath(). get_class($leaf).PHP_EOL;
+            }
+        }
+    }
+
+}
